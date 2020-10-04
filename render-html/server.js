309d75ed98c9ff99e2c922de0fc1d359e6fa7f40
John King <Johnk858@ymail.com>
2020-10-04 20:55:19 +0200

    HEROKU BROKE GITHUB
    Heroku scrapped. Rebuilt Github Desktop app

---
+++ render-html/server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs= require('fs');
+
+function onRequest(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  fs.readfile('./index.html', null, function(error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write('File not found!');
+    } else {response.write(data);}
+    response.end();
+  });
+  response.end();
+}
+
+http.createServer(onRequest).listen(8000);
