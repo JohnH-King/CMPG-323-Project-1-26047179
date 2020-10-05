4018875973d592225b9579c271d5c24253a067a9
John King <Johnk858@ymail.com>
2020-10-06 00:32:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ var fs = require('fs');
 const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
 
-const server = http.createServer(function(req, res) {
-  fs.readFile('hindex.html', function(err, data) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(data);
-  res.end();
-  });
-});
+const server = http.createServer(app);
 
 server.listen(port, () => {
 console.log("Listening on Port 5000");
