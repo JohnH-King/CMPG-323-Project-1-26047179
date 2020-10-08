3b21e45c856a49ee41d7075a398ec2f5dff65fea
John King <Johnk858@ymail.com>
2020-10-08 15:41:00 +0200

    Final CV loaded

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ const proxyurl = 'https://cors-anywhere.herokuapp.com/'; //server for proxy, no 
 var mybutton = document.getElementById("scrollToTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction()
-};
+//window.onscroll = function() {
+//  scrollFunction()
+//};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = 'none';
-  } else {
-    mybutton.style.display = 'none';
-  }
-}
+//function scrollFunction() {
+//  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//    mybutton.style.display = 'none';
+//  } else {
+//    mybutton.style.display = 'none';
+//  }
+//}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
